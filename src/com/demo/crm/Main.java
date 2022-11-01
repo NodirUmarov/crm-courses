14a00894846d9bcf1c6de4bc36c55672b598c510
Skward18 <elbek1987.26.07@mail.ru>
2022-11-02 04:00:33 +0500

    New Service added

--- src/com/demo/crm/Main.java
+++ src/com/demo/crm/Main.java
@@ -1,17 +1,31 @@
 package com.demo.crm;
 
+import com.demo.crm.model.Course;
+import com.demo.crm.model.CourseType;
+import com.demo.crm.model.Mentor;
 import com.demo.crm.model.Student;
+import com.demo.crm.service.CourseService;
+import com.demo.crm.service.CourseTypeService;
+import com.demo.crm.service.MentorService;
 import com.demo.crm.service.StudentService;
 
 public class Main {
     public static void main(String[] args) {
-        StudentService studentService = new StudentService(); //
+        StudentService studentService = new StudentService();
         Student student = studentService.create();
 
-        StudentService studentService1 = new StudentService();
-        Student student1 = studentService1.create();
+        MentorService mentorService = new MentorService();
+        Mentor mentor = mentorService.create();
+
+        CourseService courseService = new CourseService();
+        Course course = courseService.create();
+
+        CourseTypeService courseTypeService = new CourseTypeService();
+        CourseType courseType = courseTypeService.create();
 
         System.out.println(student);
-        System.out.println(student1);
+        System.out.println(mentor);
+        System.out.println(course);
+        System.out.println(courseType);
     }
 }
